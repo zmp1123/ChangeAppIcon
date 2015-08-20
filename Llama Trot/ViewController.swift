8ae4a8c9b6708a348c1fec77ab9b7d0aa759d153
zmp1123 <841777471@qq.com>
2015-08-20 17:22:12 +0800

    init

---
+++ Llama Trot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Llama Trot
+//
+//  Created by zmp1123 on 8/20/15.
+//  Copyright (c) 2015 zmp1123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Llama TrotTests/Llama_TrotTests.swift
@@ -0,0 +1,36 @@
+//
+//  Llama_TrotTests.swift
+//  Llama TrotTests
+//
+//  Created by zmp1123 on 8/20/15.
+//  Copyright (c) 2015 zmp1123. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Llama_TrotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
